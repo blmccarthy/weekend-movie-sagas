da8febe241c5554b0e65fc9c0c8a4d234a5171c8
Brant McCarthy <brant.mccarthy@me.com>
2022-03-06 14:57:42 -0600

    added MUI Card + Typography, plus basic CSS centering for MovieList Component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,6 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
+// MATERIAL UI
+import {
+    Box, Card, CardActions, CardContent, Button, Typography, CardMedia
+} from '@mui/material';
+
 
 function MovieList() {
 
@@ -21,11 +26,11 @@ function MovieList() {
 
         // The 'event' = movie.id of image that was clicked
         // The [0] makes it a simple object, rather than array of 1 object
-        const selectedMovie = (movies.filter( movie => movie.id == event))[0]
+        const selectedMovie = (movies.filter(movie => movie.id == event.id))[0];        
 
         // Stores Selected Movie info
-        dispatch({ type: 'SET_SELECTED_MOVIE', payload: selectedMovie})
-        history.push(`/Details/${selectedMovie.id}`)
+        dispatch({ type: 'SET_SELECTED_MOVIE', payload: selectedMovie });
+        history.push(`/Details/${selectedMovie.id}`);
 
         // TODO: *Strech
         // Current solution breaks '/Details' on refresh
@@ -35,12 +40,14 @@ function MovieList() {
     const navMovieForm = () => {
 
         // Clears Selected Movie Reducer so form doesn't preload with information
-        dispatch({ type: 'SET_SELECTED_MOVIE', payload: {
-            title: '',
-            poster: '',
-            description: '',
-            genre: ''
-        } });
+        dispatch({
+            type: 'SET_SELECTED_MOVIE', payload: {
+                title: '',
+                poster: '',
+                description: '',
+                genre: ''
+            }
+        });
         history.push('/form')
     }
 
@@ -50,16 +57,32 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img 
-                                // function below: e.target.id = {movie.id}
-                                onClick={(e) => handleImageClick(e.target.id)} 
-                                src={movie.poster} 
+                        <Card 
+                            key={movie.id} 
+                            sx={{ 
+                                width: 225,
+                                margin: 1,                                
+                            }}
+                            onClick={(e) => handleImageClick(e.target)}
+                        >
+                            {/* <h3>{movie.title}</h3> */}
+                            <Typography 
+                                variant="h6" 
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                                sx={{p: 1.5, height: 60}}
+                            >
+                                {movie.title}
+                            </Typography>
+                            <CardMedia
+                                component="img"
+                                // height="300"
+                                image={movie.poster}
                                 alt={movie.title}
-                                id={movie.id} // event target ^^^
+                                id={movie.id}
                             />
-                        </div>
+                        </Card>
                     );
                 })}
             </section>
