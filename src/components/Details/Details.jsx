b28d23b136e600ec3b9f860a236f36d95e49a287
Brant McCarthy <brant.mccarthy@me.com>
2022-03-05 18:49:03 -0600

    implemented .filter on <details> to resolve display bug

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -7,20 +7,23 @@ export default function Details() {
     const movies = useSelector(store => store.movies);
     const selectedMovieReducer = useSelector(store => store.selectedMovieReducer);
 
-    // Targets specific movie that was clicked
-    // Pulls info from the 'movies' Reducer
-    const selectedMovie = movies[selectedMovieReducer - 1];
+    // Targets specific movie that was clicked from Reducer
+    const selectedMovie = movies.filter(movie => movie.id == selectedMovieReducer);
 
     const handleBack = () => {
         history.push('/');
     }
 
+    console.log('movies', movies);
+    console.log('selectedMovieReducer', selectedMovieReducer);
+    console.log('selectedMovie', selectedMovie);
+
     return (
         <>
             <div>
-                <img src={selectedMovie.poster} />
-                <h1>{selectedMovie.title}</h1>
-                <div>{selectedMovie.description}</div>
+                <img src={selectedMovie[0].poster} />
+                <h1>{selectedMovie[0].title}</h1>
+                <div>{selectedMovie[0].description}</div>
                 <br />
                 <button onClick={handleBack}>Back</button>
             </div>
--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ function* fetchAllMovies() {
         const movies = yield axios.get('/api/movie');
         console.log('get all:', movies.data);
         yield put({ type: 'SET_MOVIES', payload: movies.data });
-
     } catch {
         console.log('get all error');
     }
