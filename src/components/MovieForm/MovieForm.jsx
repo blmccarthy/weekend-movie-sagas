cba0dc531c5e60dee623a4527a71ceec205e0565
Brant McCarthy <brant.mccarthy@me.com>
2022-03-06 20:46:27 -0600

    added MUI styling to MovieForm

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 
 // MATERIAL UI
-import { Button } from '@mui/material';
+import { Button, TextField, Select, MenuItem, Paper, FormControl, Typography, InputLabel, Grid } from '@mui/material';
 
 export default function MovieForm() {
 
@@ -13,14 +13,14 @@ export default function MovieForm() {
     const genres = useSelector(store => store.genres)
     const movies = useSelector(store => store.movies)
     const movieGenres = useSelector(store => store.movieGenres)
-    
+
     // Imports Selected Movie Details
     const selectedMovie = useSelector(store => store.selectedMovieReducer)
 
     // Genres for dropdown made available if page refreshes 
     useEffect(() => {
-        dispatch({ type: 'FETCH_GENRES'})
-    }, []) ;
+        dispatch({ type: 'FETCH_GENRES' })
+    }, []);
 
     const [newMovieTitle, setNewMovieTitle] = useState('')
     const [newMoviePoster, setNewMoviePoster] = useState('')
@@ -47,44 +47,69 @@ export default function MovieForm() {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    required
-                    type="text"
-                    placeholder="Movie title"
-                    value={newMovieTitle}
-                    onChange={(e) => setNewMovieTitle(e.target.value)}
-                />
-                <input
-                    required
-                    type="text"
-                    placeholder="Movie Poster URL"
-                    value={newMoviePoster}
-                    onChange={(e) => setNewMoviePoster(e.target.value)}
-                />
-                <input
-                    required
-                    type="text"
-                    placeholder="Movie Description"
-                    value={newMovieDescription}
-                    onChange={(e) => setNewMovieDescription(e.target.value)}
-                />
-                <select
-                    required
-                    name="Movie Genre"
-                    id="genre"
-                    value={newMovieGenre}
-                    onChange={(e) => setNewMovieGenre(e.target.value)}
-                >
-                    <option value="" default hidden>Select a Genre...</option>
-                    {genres.map((genre) => {
-                        return (<option key={genre.id} value={genre.id}>{genre.name}</option>)
-                    })}
-                </select>
-                <br/><br/>
-                <Button variant="contained" type="submit" sx={{m: 1}}>Save</Button>
-                <Button variant="outlined" onClick={handleCancel} sx={{m: 1}}>Cancel</Button>
-            </form>
+            <Paper elevation={3} sx={{ m: 10, p: 10 }}>
+                <Typography variant="h4" sx={{ mb: 4 }}>
+                    Enter Movie Info
+                </Typography>
+                <Grid container spacing={3}>
+                    <Grid item lg={6} md={6} sm={12}>
+                        <TextField
+                            required
+                            type="text"
+                            placeholder="Movie title"
+                            value={newMovieTitle}
+                            onChange={(e) => setNewMovieTitle(e.target.value)}
+                            sx={{ width: "100%" }}
+                        />
+                    </Grid>
+                    <Grid item lg={6} md={6} sm={12}>
+                        <TextField
+                            required
+                            type="text"
+                            placeholder="Movie Poster URL"
+                            value={newMoviePoster}
+                            onChange={(e) => setNewMoviePoster(e.target.value)}
+                            sx={{ width: "100%" }}
+                        />
+                    </Grid>
+                    <Grid item lg={6} md={6} sm={12}>
+                        <TextField
+                            required
+                            type="text"
+                            placeholder="Movie Description"
+                            value={newMovieDescription}
+                            onChange={(e) => setNewMovieDescription(e.target.value)}
+                            sx={{ width: "100%" }}
+                        />
+                    </Grid>
+                    <Grid item lg={6} md={6} sm={12}>
+                        <FormControl sx={{ width: "100%" }}>
+                            <InputLabel id="movie-genre">Movie Genre</InputLabel>
+                            <Select
+                                required
+                                name="Movie Genre"
+                                id="genre"
+                                label="Movie Genre"
+                                placeholder="Movie Genre"
+                                value={newMovieGenre}
+                                onChange={(e) => setNewMovieGenre(e.target.value)}
+                            >
+                                <MenuItem value="" default disabled>Select a Genre...</MenuItem>
+                                {genres.map((genre) => {
+                                    return (<MenuItem key={genre.id} value={genre.id}>{genre.name}</MenuItem>)
+                                })}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                        <br />
+                    <Grid item lg={6} md={6} sm={12}>
+                        <Button variant="contained" onClick={handleSubmit} sx={{ width: "100%" }}>Save</Button>
+                    </Grid>
+                    <Grid item lg={6} md={6} sm={12}>
+                        <Button variant="outlined" onClick={handleCancel} sx={{ width: "100%" }}>Cancel</Button>
+                    </Grid>
+                </Grid>
+            </Paper>
         </>
     )
 }
